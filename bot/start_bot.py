d35f9a2307097f40f28ff781636376b658e717eb
Arseniy <94193304+ArseniyKvasov@users.noreply.github.com>
2025-09-24 17:02:15 +0300

    Delete bot directory

--- bot/start_bot.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bot import dp, bot
-import handlers  # подключаем, чтобы хендлеры зарегистрировались
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
